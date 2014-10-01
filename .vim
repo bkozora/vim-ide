971d5c40682306eaffaa53cf31d9f7cf055b1f57
Robert Kozora <robert.kozora@gearylsf.com>
2014-10-01 17:33:50 -0400

    modified install script, added in airline submodules

---
+++ .vim
@@ -0,0 +1,1 @@
+/home/bobby/vim-ide
--- install.sh
+++ install.sh
@@ -4,16 +4,19 @@
 if [ -d ~/lsfinteractive_vim-ide ]
     then
         echo "Moving ~/lsfinteractive_vim-ide to ~/vim-ide..."
+        cd ~/
         mv ~/lsfinteractive_vim-ide ~/vim-ide
 fi
 
 if [ -d ~/vim-ide ]
     then
+        cd ~/vim-ide
+
         # Set up our git submodule dependencies
         git submodule init
         git submodule update
 
-        printf "\n\n\nInitialized submodule plugins...\n\n\n"
+        printf "\nInitialized submodule plugins...\n"
 
         # backup our existing configurations
         if [ -f ~/.vimrc ]
@@ -33,11 +36,12 @@ if [ -d ~/vim-ide ]
         # Symlink to our new vim configuration
         ln -s ~/vim-ide/.vimrc ~/.vimrc
         ln -s ~/vim-ide ~/.vim
-        echo "Created symlinks to point Vim to our new kickass environment..."
+        echo "\nCreated symlinks to point Vim to our new kickass environment..."
 
         # Create file for local customizations
         touch ~/.vimrc.local
-        printf "\n\nCreated ~/.vimrc.local Place any customizations in there to override the default configurations set up"
+        printf "\nCreated ~/.vimrc.local Place any customizations in there to override the default configurations set
+        up\n\n"
 
 fi
 
