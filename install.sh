64a1eaf16843c0a1819a0df9962c9cab42a1acd2
Robert Kozora <robert.kozora@lsf-corp.com>
2014-10-01 14:02:22 -0400

    fixed some syntax errors

--- install.sh
+++ install.sh
@@ -2,33 +2,42 @@
 
 # Rename our ugly default unfuddle directory
 if [ -d ~/lsfinteractive_vim-ide ]
-    mv ~/lsfinteractive_vim-ide ~/vim-ide
+    then
+        echo "Moving ~/lsfinteractive_vim-ide to ~/vim-ide..."
+        mv ~/lsfinteractive_vim-ide ~/vim-ide
 fi
 
 if [ -d ~/vim-ide ]
-
-    # Set up our git submodule dependencies
-    git submodule init
-    git submodule update
-
-    # backup our existing configurations
-    if [ -f ~/.vimrc ]
     then
-        # Backup our .vimrc
-        mv ~/.vimrc ~/.vimrc-old
-    fi
+        # Set up our git submodule dependencies
+        git submodule init
+        git submodule update
 
-    if [ -d ~/.vim ]
-    then
-        # Backup our old .vim directory
-        mv ~/.vim ~/.vim-old
-    fi
+        printf "\n\n\nInitialized submodule plugins...\n\n\n"
 
-    # Symlink to our new vim configuration
-    ln -s ~/vim-ide/.vimrc ~/.vimrc
-    ln -s ~/vim-ide ~/.vim
+        # backup our existing configurations
+        if [ -f ~/.vimrc ]
+        then
+            # Backup our .vimrc
+            echo "Backed up your existing ~/.vimrc to ~/.vimrc-old"
+            mv ~/.vimrc ~/.vimrc-old
+        fi
 
-    # Create file for local customizations
-    touch ~/.vimrc.local
+        if [ -d ~/.vim ]
+        then
+            # Backup our old .vim directory
+            echo "Backed up your existing ~/.vim directory to ~/.vim-old"
+            mv ~/.vim ~/.vim-old
+        fi
+
+        # Symlink to our new vim configuration
+        ln -s ~/vim-ide/.vimrc ~/.vimrc
+        ln -s ~/vim-ide ~/.vim
+        echo "Created symlinks to point Vim to our new kickass environment..."
+
+        # Create file for local customizations
+        touch ~/.vimrc.local
+        printf "\n\nCreated ~/.vimrc.local Place any customizations in there to override the default configurations set up"
 
 fi
+
