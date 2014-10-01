5226ab53f2d9bfbe4c9ac2557b682ab20de91d8c
Melissa Kielty <mkielty@lsfinteractive.com>
2014-10-01 12:13:51 -0400

    added in logic to properly create symlinks and rename the ugly unfuddle naming convention

--- install.sh
+++ install.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-
-# Set up our git submodule dependencies
-git submodule init
-git submodule update
-
-# backup our existing configurations
-if [ -f ~/.vimrc ]
-then
-    # Backup our .vimrc
-    mv ~/.vimrc ~/.vimrc-old
+# Rename our ugly default unfuddle directory
+if [ -d ~/lsfinteractive_vim-ide ]
+    mv ~/lsfinteractive_vim-ide ~/vim-ide
 fi
 
-if [ -d ~/.vim ]
-then
-    # Backup our old .vim directory
-    mv ~/.vim ~/.vim-old
+if [ -d ~/vim-ide ]
+
+    # Set up our git submodule dependencies
+    git submodule init
+    git submodule update
+
+    # backup our existing configurations
+    if [ -f ~/.vimrc ]
+    then
+        # Backup our .vimrc
+        mv ~/.vimrc ~/.vimrc-old
+    fi
+
+    if [ -d ~/.vim ]
+    then
+        # Backup our old .vim directory
+        mv ~/.vim ~/.vim-old
+    fi
+
+    # Symlink to our new vim configuration
+    ln -s ~/vim-ide/.vimrc ~/.vimrc
+    ln -s ~/vim-ide ~/.vim
+
+    # Create file for local customizations
+    touch ~/.vimrc.local
+
 fi
-
-# Symlink to our new vim configuration
-ln -s .vimrc ~/.vimrc
-ln -s . ~/.vim
-
-# Create file for local customizations
-touch ~/.vimrc.local
